f1fd14af6797f03886764e7641183ca2ed8b4f26
cdluzhichao <cdluzhichao@jd.com>
2018-11-02 10:51:50 +0800

    feat 1. 项目结构改造

---
+++ client/app.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="app">
+        {{text}}
+        <div id="cover"></div>
+        <Header></Header>
+        <todo></todo>
+        <foot></foot>
+    </div>
+
+</template>
+
+<script>
+    import Header from './layout/header'
+    import Foot from './layout/footer.jsx'
+    import Todo from './views/todo/todo'
+
+    export default {
+        components: {
+            Header,
+            Foot,
+            Todo
+        },
+        data() {
+            return {
+                text: '我要天天吃米线！！'
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+    #app{
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom 0
+    }
+    #cover{
+        position absolute
+        left 0
+        top 0
+        right 0
+        bottom 0
+        background-color #999
+        opacity .9
+        z-index -1
+    }
+</style>
---
+++ client/layout/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main-header">
+        <h1>JTodo</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped lang="stylus">
+    .main-header{
+        text-align center
+        h1{
+            font-size 120px
+            color: gold
+            font-weight 100
+            margin 20px
+        }
+    }
+</style>
